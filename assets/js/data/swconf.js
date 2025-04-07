6f25eae1dddbc7643033b7fdce833211957cdaf0
Tuan Nguyen <nqtuan0192@gmail.com>
2025-04-08 00:53:57 +0900

    Update

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1744038276',resources: [
+    cacheName: 'chirpy-1744041237',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
