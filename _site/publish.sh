1bb5269a55234a7436af654d167f8adb8e1bb7d9
Tuan Nguyen <nqtuan0192@gmail.com>
2025-04-08 01:07:12 +0900

    update

---
+++ _site/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# set variable for tuannq-nii.github.io
+OUT_DIR="blogs"
+
+# Generate static pages
+bundle exec jekyll build -d $OUT_DIR
+
+# Copy DATA files to OUt_DIR
+# cp DATA/CNAME $OUT_DIR
+
+# Commit and push to github
+cd $OUT_DIR
+git add .
+git commit -m "Update"
+git push -f origin main
+
